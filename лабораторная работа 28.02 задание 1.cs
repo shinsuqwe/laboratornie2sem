89cc50e03d96a08c38f008f66083a99217442fc0
dddffaa <hatpoint@bk.ru>
2025-04-06 14:17:52 +0600

    Add files via upload

---
+++ лабораторная работа 28.02 задание 1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите постфиксное выражение: ");
+        string input = Console.ReadLine();
+        string[] tokens = input.Split(new Char[] { ' ' });
+        Stack stack = new Stack();
+        bool check = true;
+        string operators = "+-*/";
+        foreach (string token in tokens)
+        {
+            if (Number(token))
+            {
+                stack.Push(double.Parse(token));
+            }
+            else if (operators.Contains(token))
+            {
+                if (stack.Count < 2)
+                {
+                    check = false;
+                    break;
+                }
+                double operand2 = (double)stack.Pop();
+                double operand1 = (double)stack.Pop();
+                double result = Operation(token, operand1, operand2);
+                stack.Push(result);
+            }
+            else
+            {
+                check = false;
+                break;
+            }
+        }
+        if (check && stack.Count == 1)
+        {
+            Console.WriteLine($"Результат: {stack.Pop()}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректное выражение.");
+        }
+    }
+    static bool Number(string token)
+    {
+        return double.TryParse(token, out _);
+    }
+    static double Operation(string operation, double operand1, double operand2)
+    {
+        switch (operation)
+        {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0)
+                {
+                    Console.WriteLine("Деление на ноль.");
+                    Environment.Exit(1);
+                }
+                return operand1 / operand2;
+            default:
+                Console.WriteLine($"Неизвестный оператор '{operation}'.");
+                Environment.Exit(1);
+                return 0;
+        }
+    }
+}
---
+++ лабораторная работа 28.02 задание 2.cs
@@ -0,0 +1,38 @@
+﻿/* На вход подается список из целых положительных элементов. Необходимо
+выдать элементы, с помощью которых составлен список, и частоту появления
+каждого элемента. */
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        Console.Write("Введите количество элементов списка: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            list.Add(a);
+        }
+        list.Sort();
+        Dictionary<int, int> rate = new Dictionary<int, int>();
+        foreach (int el in list)
+        {
+            if (rate.ContainsKey(el))
+            {
+                rate[el]++;
+            }
+            else
+            {
+                rate[el] = 1;
+            }
+        }
+        Console.WriteLine("Элементы и их частота появления:");
+        foreach (var pair in rate)
+        {
+            Console.WriteLine($"Элемент {pair.Key}: {pair.Value} раз(-а)");
+        }
+    }
+}
