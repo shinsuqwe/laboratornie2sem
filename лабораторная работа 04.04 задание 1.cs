7b2f3efb47c38f63f57ce41502ebd42a68354a68
dddffaa <hatpoint@bk.ru>
2025-04-29 22:24:58 +0600

    Add files via upload

---
+++ лабораторная работа 04.04 задание 1.cs
@@ -0,0 +1,20 @@
+﻿/* Лабораторная за 04.04
+Необходимо с помощью лямбда-выражений вычислить сумму, произведение, разность и деление
+между двумя переменными. */
+delegate void Addition(int a, int b);
+delegate void Difference(int a, int b);
+delegate void Multiplication(int a, int b);
+delegate void Division(int a, int b);
+class Expression
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите числа (2 должно быть целым)");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        Addition add = (a, b) => Console.WriteLine($"Сложеение: {a} + {b} = {a + b}"); add(a, b);
+        Difference dif = (a, b) => Console.WriteLine($"Разность: {a} - {b} = {a - b}"); dif(a, b);
+        Addition multi = (a, b) => Console.WriteLine($"Умножение: {a} * {b} = {a * b}"); multi(a, b);
+        Addition div = (a, b) => Console.WriteLine($"Деление: {a} / {b} = {a / b}"); div(a, b);
+    }
+}
---
+++ лабораторная работа 04.04 задание 2.cs
@@ -0,0 +1,25 @@
+﻿/* Лабораторная работа №2 за 04.04
+Дан список, состоящий из слов. Необходимо с помощью лямбда-выражений реализовать выборку слов,
+которые начинаются на символ а.*/
+delegate void Words(List<string> l);
+class WordsA
+{
+    static void Main()
+    {
+        List<string> words = new List<string>();
+        Console.Write("Укажите количество слов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите слово {i+1}: ");
+            words.Add(Console.ReadLine());
+        }
+        foreach (string word in words)
+        {
+            if (word.Length > 0 && word[0] == 'а' || word.Length > 0 && word[0] == 'А')
+            {
+                Console.WriteLine($"Слова, начинающиеся на А или а: {word}");
+            }
+        }
+    }
+}
